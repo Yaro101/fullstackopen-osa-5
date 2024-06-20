35a17998e7c9bc7a2560a2dc0529172d4c129af3
Yahya Rouissi <yahyarouissi@gmail.com>
2024-06-20 10:34:12 +0300

    5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,12 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    Title: {blog.title} <br />
-    Author: {blog.author} <br />
-    Link: {blog.url} <br />
-    Likes: {blog.likes} <br />
-    {blog.user.name} <br />
-    <br />
-  </div>
-);
+import React, { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}{" "}
+        <button onClick={toggleVisibility}>{visible ? "hide" : "show"}</button>
+      </div>
+      {visible && (
+        <div>
+          <p>{blog.url}</p>
+          <div>
+            Likes {blog.likes} <button>like</button>
+          </div>
+          <p>{blog.user.name}</p>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
